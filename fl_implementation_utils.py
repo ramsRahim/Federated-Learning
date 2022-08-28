256f1f612d1fed14a913a96c373e8a6ca16e305c
Rahim Hossain <rahimhossain23@gmail.com>
2022-08-29 02:18:39 +0600

    Federated Learning step by step implemented

---
+++ fl_implementation_utils.py
@@ -0,0 +1,131 @@
+import numpy as np
+import random
+import os
+import pickle 
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras import backend as K
+
+
+
+def load(paths):
+   
+    data = list()
+    labels = list()
+    rawData = pickle.load(open('paths', 'rb'))
+
+    keys=rawData.keys()
+    data = rawData[keys[:keys.shape[0]-1]].values
+    labels = rawData[keys[keys.shape[0]-1]].values
+    return data, labels
+
+
+def create_clients(data_list, label_list, num_clients=10, initial='clients'):
+    ''' return: a dictionary with keys clients' names and value as 
+                data shards - tuple of datas and label lists.
+        args: 
+            data_list: a list of numpy arrays of training data
+            label_list:a list of binarized labels for each data
+            num_client: number of fedrated members (clients)
+            initials: the clients'name prefix, e.g, clients_1 
+            
+    '''
+
+    #create a list of client names
+    client_names = ['{}_{}'.format(initial, i+1) for i in range(num_clients)]
+
+    #randomize the data
+    data = list(zip(data_list, label_list))
+    random.shuffle(data)
+
+    #shard data and place at each client
+    size = len(data)//num_clients
+    shards = [data[i:i + size] for i in range(0, size*num_clients, size)]
+
+    #number of clients must equal number of shards
+    assert(len(shards) == len(client_names))
+
+    return {client_names[i] : shards[i] for i in range(len(client_names))}
+
+
+
+def batch_data(data_shard, bs=32):
+    '''Takes in a clients data shard and create a tfds object off it
+    args:
+        shard: a data, label constituting a client's data shard
+        bs:batch size
+    return:
+        tfds object'''
+    #seperate shard into data and labels lists
+    data, label = zip(*data_shard)
+    dataset = tf.data.Dataset.from_tensor_slices((list(data), list(label)))
+    return dataset.shuffle(len(label)).batch(bs)
+
+
+class SimpleMLP:
+    @staticmethod
+    def build(shape, classes):
+        model = Sequential()
+        model.add(Dense(200, input_shape=(shape,)))
+        model.add(Activation("relu"))
+        model.add(Dense(200))
+        model.add(Activation("relu"))
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        return model
+    
+
+def weight_scalling_factor(clients_trn_data, client_name):
+    client_names = list(clients_trn_data.keys())
+    #get the bs
+    bs = list(clients_trn_data[client_name])[0][0].shape[0]
+    #first calculate the total training data points across clinets
+    global_count = sum([tf.data.experimental.cardinality(clients_trn_data[client_name]).numpy() for client_name in client_names])*bs
+    # get the total number of data points held by a client
+    local_count = tf.data.experimental.cardinality(clients_trn_data[client_name]).numpy()*bs
+    return local_count/global_count
+
+
+def scale_model_weights(weight, scalar):
+    '''function for scaling a models weights'''
+    weight_final = []
+    steps = len(weight)
+    for i in range(steps):
+        weight_final.append(scalar * weight[i])
+    return weight_final
+
+
+
+def sum_scaled_weights(scaled_weight_list):
+    '''Return the sum of the listed scaled weights. The is equivalent to scaled avg of the weights'''
+    avg_grad = list()
+    #get the average grad accross all client gradients
+    for grad_list_tuple in zip(*scaled_weight_list):
+        layer_mean = tf.math.reduce_sum(grad_list_tuple, axis=0)
+        avg_grad.append(layer_mean)
+        
+    return avg_grad
+
+
+def test_model(X_test, Y_test,  model, comm_round):
+    cce = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    #logits = model.predict(X_test, batch_size=100)
+    logits = model.predict(X_test)
+    loss = cce(Y_test, logits)
+    acc = accuracy_score(tf.argmax(logits, axis=1), tf.argmax(Y_test, axis=1))
+    print('comm_round: {} | global_acc: {:.3%} | global_loss: {}'.format(comm_round, acc, loss))
+    return acc, loss
